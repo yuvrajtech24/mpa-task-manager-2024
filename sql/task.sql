12038913ee6f37225df3ea925956fd0faea68ab8
yuvraj singh <yuvraj@yuvrajtech.com>
2025-02-02 18:57:06 +0530

    added sql file

--- sql/task.sql
+++ sql/task.sql
@@ -1,7 +1,7 @@
 create table tasks(
     id int auto_increment primary key,
     title varchar(50) not null default 'testTitle',
-    description text not null default 'default description text',
+    description text,
     dueDate date not null default '2025-01-01',
     userId int,
     foreign key(userId) references users(id)
